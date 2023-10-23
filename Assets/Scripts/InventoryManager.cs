e179495ae9640c08a97e5e27a2b6eef677bd8917
viniciusmm7 <viniciusmm7@al.insper.edu.br>
2023-10-23 08:12:37 -0300

    testing inventory system

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour {
     
+    public int maxStackedItems = 10;
+    public InventorySlot[] inventorySlots;
     public GameObject inventoryItemPrefab;
 
+    public bool AddItem(Item item) {
+        for (int i = 0; i < inventorySlots.Length; i++) {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot != null && itemInSlot.item == item && item.stackable && itemInSlot.count < maxStackedItems && slot.type == item.type) {
+                itemInSlot.count++;
+                itemInSlot.RefreshCount();
+                return true;
+            } else if (itemInSlot == null && slot.type == item.type) {
+                SpawnNewItem(item, slot);
+                return true;
+            }
+        }
+        return false;
+    }
+
     void SpawnNewItem(Item item, InventorySlot slot) {
-        GameObject newItemGabeObject = Instantiate(inventoryItemPrefab, slot.transform);
+        GameObject newItemGameObject = Instantiate(inventoryItemPrefab, slot.transform);
+        InventoryItem inventoryItem = newItemGameObject.GetComponent<InventoryItem>();
+        inventoryItem.InitializeItem(item);
     }
 }
