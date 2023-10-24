6fb29d53125f802e43b9a3b270eafced4b3f74ed
Nery <pedronas@al.insper.edu.br>
2023-10-23 23:16:53 -0300

    Scripts branch vinicius e interação com o item pá

--- Assets/Scripts/ShovelSpawn.cs
+++ Assets/Scripts/ShovelSpawn.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class ShovelSpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Item shovelItem; // Atribuir a pá (shovel) a este campo no Inspector.
+    public GameObject shovelPrefab; // Atribuir o prefab da pá no Inspector.
+    public Vector2Int mapDimensions = new Vector2Int(500, 500);
+    public LayerMask obstacleLayer; // Definir a camada que representa obstáculos.
+
     void Start()
     {
-        
+        SpawnShovelRandomly();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Função para spawnar a pá (shovel) em uma posição aleatória sem colisões.
+    void SpawnShovelRandomly()
     {
-        
+        Vector2 randomPosition;
+        Collider2D hitCollider;
+
+        do
+        {
+            randomPosition = new Vector2(Random.Range(0, mapDimensions.x), Random.Range(0, mapDimensions.y));
+            hitCollider = Physics2D.OverlapCircle(randomPosition, 1.0f, obstacleLayer);
+        } while (hitCollider != null);
+
+        // Cria uma instância da pá (shovel) no local aleatório.
+        GameObject shovelInstance = Instantiate(shovelPrefab, randomPosition, Quaternion.identity);
+
+        // Obtém o componente InteractableObj da pá (shovel) para atribuir o item.
+        InteractableObj interactable = shovelInstance.GetComponent<InteractableObj>();
+        if (interactable != null)
+        {
+            interactable.item = shovelItem;
+        }
     }
 }
+
