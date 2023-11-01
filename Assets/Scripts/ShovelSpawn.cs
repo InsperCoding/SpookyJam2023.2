a525d5014882b0de41ab2355f596a82be9de8469
Nery <pedronas@al.insper.edu.br>
2023-10-31 23:05:39 -0300

    Spawn da pá implementado

--- Assets/Scripts/ShovelSpawn.cs
+++ Assets/Scripts/ShovelSpawn.cs
@@ -6,30 +6,44 @@ public class ShovelSpawn : MonoBehaviour
 {
     public Item shovelItem; // Atribuir a pá (shovel) a este campo no Inspector.
     public GameObject shovelPrefab; // Atribuir o prefab da pá no Inspector.
-    public Vector2Int mapDimensions = new Vector2Int(500, 500);
-    public LayerMask obstacleLayer; // Definir a camada que representa obstáculos.
+    public List<Vector3> SpawnPositions = new List<Vector3>(); // Lista de posições definidas para spawn da pá.
 
     void Start()
     {
-        SpawnShovelRandomly();
+        if (NoShovelsPresent()){
+            SpawnPositionsUpdate();
+            SpawnShovel();
+        }
     }
 
-    // Função para spawnar a pá (shovel) em uma posição aleatória sem colisões.
-    void SpawnShovelRandomly()
+    bool NoShovelsPresent()
     {
-        Vector2 randomPosition;
-        Collider2D hitCollider;
+        // Verifica se não há pás presentes no momento
+        GameObject[] existingShovels = GameObject.FindGameObjectsWithTag("ShovelTag"); // Tag para identificar a pá
 
-        do
-        {
-            randomPosition = new Vector2(Random.Range(0, mapDimensions.x), Random.Range(0, mapDimensions.y));
-            hitCollider = Physics2D.OverlapCircle(randomPosition, 1.0f, obstacleLayer);
-        } while (hitCollider != null);
+        // Não há pás no mapa
+        return existingShovels.Length == 0;
+    }
 
-        // Cria uma instância da pá (shovel) no local aleatório.
-        GameObject shovelInstance = Instantiate(shovelPrefab, randomPosition, Quaternion.identity);
+    void SpawnPositionsUpdate()
+    {
+        // Define lista de posições fixas da pá
+        SpawnPositions.Add(new Vector3(27.092f, 1.54f, -33.79f));
+        SpawnPositions.Add(new Vector3(52.76f, 1.54f, 34.33f));
+        SpawnPositions.Add(new Vector3(37.452f, 1.54f, 46.68f));
+        SpawnPositions.Add(new Vector3(-8.4f, 1.54f, 5.72f));
+        SpawnPositions.Add(new Vector3(47.34f, 1.54f, -49.3f));
+    }
 
-        // Obtém o componente InteractableObj da pá (shovel) para atribuir o item.
+    // Função para spawnar a pá (shovel) em uma posição definida previamente.
+    void SpawnShovel()
+    {
+        int randomNumber = UnityEngine.Random.Range(0, SpawnPositions.Count);
+        Vector3 posição = SpawnPositions[randomNumber];
+        
+        // Aqui você precisará instanciar o objeto shovelPrefab na posição escolhida
+        GameObject shovelInstance = Instantiate(shovelPrefab, posição, Quaternion.identity);
+
         InteractableObj interactable = shovelInstance.GetComponent<InteractableObj>();
         if (interactable != null)
         {
