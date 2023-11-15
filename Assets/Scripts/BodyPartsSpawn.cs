d54a247a82acf73d7a85eceb2c9c0c2e2323828e
Nery <pedronas@al.insper.edu.br>
2023-11-14 23:46:08 -0300

    Prefabs e correções no script BodyPartsSpawn

--- Assets/Scripts/BodyPartsSpawn.cs
+++ Assets/Scripts/BodyPartsSpawn.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShovelSpawn : MonoBehaviour
+public class BodyPartsSpawn : MonoBehaviour
 {
-    public Item shovelItem; // Atribuir a pá (shovel) a este campo no Inspector.
-    public GameObject shovelPrefab; // Atribuir o prefab da pá no Inspector.
-    public List<Vector3> BodyPositions = new List<Vector3>(); // Lista de posições definidas para spawn da pá.
+    public Item bpItem;
+    public GameObject BodyPartsPrefab;
+    public List<Vector3> BodyPositions = new List<Vector3>(); // Lista de posições definidas para spawn de uma parte do corpo
+    int contador = 1;
+    List<Vector3> BodyPositionsUsed = new List<Vector3>(); // Lista de posições já utilizadas para spawn de uma parte do corpo
 
     // Implementar uma parte do código que impeça a repetição de certas partes do corpo spawnadas
     // Fazer testes com "bolinhas" enquanto estiver sem os assets dos corpos
     void Start()
     {
-        if (BodiesPresent == 6){
+        int B = BodiesPresent();
+        if (B == 6){
             SpawnPositionsUpdate();
             SpawnBodyPart();
         }
@@ -39,13 +42,22 @@ public class ShovelSpawn : MonoBehaviour
         BodyPositions.Add(new Vector3(14.02f, -1.5f, -5.42f));
         BodyPositions.Add(new Vector3(67f, -1.5f, -24.2f));
         BodyPositions.Add(new Vector3(-48.92f, 25.6f, -22.18f));
-        BodyPositions.Add(new Vector3(???, ???, ???));
+        BodyPositions.Add(new Vector3(100f, 0.7f, -30f));
+        // Adicionar mais posições possíveis
     }
 
     // Função para spawnar os corpos em uma posições definidas previamente.
     void SpawnBodyPart()
     {
-        // Aqui você precisará instanciar o objeto Prefab??? na posição escolhida
-        GameObject shovelInstance = Instantiate(Prefab???, posição, Quaternion.identity);
+        while (contador <= 6){
+            int randomNumber = UnityEngine.Random.Range(0, BodyPositions.Count);
+            Vector3 posição = BodyPositions[randomNumber];
+            if (!BodyPositionsUsed.Contains(posição)){
+                BodyPositionsUsed.Add(posição);
+                // Aqui você precisará instanciar o objeto BodypartsPrefab na posição escolhida
+                GameObject bpInstance = Instantiate(BodyPartsPrefab, posição, Quaternion.identity);
+                contador++;
+            }
+        }
     }
 }
